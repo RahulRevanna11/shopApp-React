8d38716f4e52d7281df1ad4c8fe468ccc98745ee
RahulRevanna11 <rahulrevanna11@gmail.com>
2023-11-22 14:47:40 +0530

    ui updated

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -9,7 +9,7 @@ const Navbar=()=>
         <div>
             <div  className='flex  justify-between items-center h-20 max-w-6xl mx-auto'>
                 <NavLink to='/' className="ml-5">
-                <img src="../logo.png" className="h-14"/>
+                <h1 className='text-white'>RR</h1>
 
                 </NavLink>
                 <div className='flex item-center font-medium text-slate-100 mr-5 space-x-6 ' >
